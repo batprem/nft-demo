913c93ef84621b5854b91bb1cddc9a969cb037d3
pc@zrch.com <pc@zrch.com>
2021-12-30 01:36:08 +0700

    feat: Add essential contracts

--- contracts/SimpleCollectable.sol
+++ contracts/SimpleCollectable.sol
@@ -6,3 +6,25 @@ pragma solidity 0.6.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+
+
+contract SimpleCollectable is ERC721{
+    uint256 public tokenCounter = 0;
+    constructor() public ERC721 ("Dogie", "DOG"){
+        
+    }
+
+    function createCollectable(string memory tokenURI) public returns  (uint256) {
+        // Assign TokenId to the new owner
+        // _safeMint funcation protect NFT replacement
+        uint256 newTokenId = tokenCounter;
+        _safeMint(
+            msg.sender, // Address of who calls this function
+            newTokenId // Token 0, 1, 2, ....
+        );
+        _setTokenURI(newTokenId, tokenURI);
+        tokenCounter++;
+        return newTokenId;
+    }
+    // Token URI is resource identify like http or ipfs or any url
+}
